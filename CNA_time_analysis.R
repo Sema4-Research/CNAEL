ec482a6ed10e93ea2661f4fbc6ff822ec81c73af
Wan-Yifei <yifei.wan@sema4.com>
2022-10-11 19:25:42 +0000

    Feat: New version file: CNA_time_anaylysis.R

--- CNA_time_analysis.R
+++ CNA_time_analysis.R
@@ -3,35 +3,34 @@ curation_time<- read.csv(file = '~/Library/Mobile Documents/com~apple~CloudDocs/
 #########################################################################
 # 1. test of normality. R's default is the Shapiro-Wilk Test
 #########################################################################
-
 ## if p-value > 0.05, we cannot reject the null hypothesis that the variable `len` is normally distributed,
 ## so we assume normality
-ori_time <- subset(curation_time, group=="ori")
+sbs_time <- subset(curation_time, group=="sbs")
 cnael_time <- subset(curation_time, group=="cnael")
 
-shapiro.test(ori_time$time) # note that in R, you can call a variable in a dataframe using `$`
+shapiro.test(sbs_time$time) # note that in R, you can call a variable in a dataframe using `$`
 shapiro.test(cnael_time$time) # note that in R, you can call a variable in a dataframe using `$`
 
 #########################################################################
-# 2. test of equal variance to see which t-test is more appropriate to use
+# 2. data is not normally distributed, use the wilcoxon rank-sum test
+######################################################################### 
+
+median(sbs_time$time)
+quantile(sbs_time$time)
+median(cnael_time$time)
+quantile(cnael_time$time)
+
+wilcox.test(time ~ group, data = curation_time,exact = FALSE,alternative = "two.sided") 
+
 #########################################################################
-
-## if p-value > 0.05, we cannot reject the null hypothesis that the variable `len` has equal variance
-var.test(time ~ group, data =curation_time , 
-         alternative = "two.sided")
-# With normality and equal variance assumptions met, we can use the classic t-test
+# 3. Violin Plots to see how the data value distributed
 #########################################################################
-# 3. two-sample t-test (observations are not paired).
-#########################################################################
-
-## if the variance of the two groups are same, set var.equal = TRUE, and the classic t-test is used
-t.test(ori_time$time, cnael_time$time, var.equal = TRUE)
-
-# if the variance of the two groups are not equal, set var.equal = FALSE, then Welch's test is used 
-t.test(ori_time$time, cnael_time$time, var.equal = FALSE)
-
-# if data is not normally distributed, use the wilcoxon rank-sum test
-wilcox.test(time ~ group, data = curation_time,exact = FALSE,alternative = "greater") 
+library(vioplot)
+x1 <- sbs_time$time
+x2 <- cnael_time$time
+vioplot(x1, x2, names=c("sbs", "cnael"),
+        col="gold")
+title("Violin Plots: Mins of CNA Review")
 
 
 
